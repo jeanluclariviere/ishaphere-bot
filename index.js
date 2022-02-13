cf7b6def94e24aa7f92f33d253e1a1e9503dd6a2
Jean Luc Lariviere-Lacombe <jeanluclariviere@gmail.com>
2022-02-13 01:15:18 -0500

    Renaming index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client, Collection, Intents, MessageActionRow,MessageSelectMenu } = require('discord.js');
+const { limitChannel, token } = require('./config.json');
+
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Retrieve list of commands
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// Retrieve list of events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	try {
+
+	}
+	catch (error) {
+
+	}
+	if (limitChannel){
+		if (interaction.channelId != limitChannel) {
+			limitChannelName = client.channels.cache.get(limitChannel).name
+			await interaction.reply({ content: 'Insufficient privileges - Bot has been restricted to channel #' + limitChannelName + ".", ephemeral: true });
+			return;
+			
+		}
+	}
+
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
+
